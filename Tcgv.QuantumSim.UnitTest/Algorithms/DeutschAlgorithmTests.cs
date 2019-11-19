6170a96f9c954255f8fb377fd11bba641f3bcf0b
Thomas Vilhena <thomas.vilhena@gmail.com>
2019-11-19 00:33:09 -0300

    Implementing DeutschAlgorithms.cs

---
+++ Tcgv.QuantumSim.UnitTest/Algorithms/DeutschAlgorithmTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+using Tcgv.QuantumSim.Operations;
+
+namespace Tcgv.QuantumSim.Algorithms
+{
+    [TestClass()]
+    public class DeutschAlgorithmTests
+    {
+        [TestMethod()]
+        public void BalancedGateTest()
+        {
+            var algo = new DeutschAlgorithm();
+
+            Assert.IsTrue(algo.IsBalanced(new BalancedGate()));
+        }
+
+        [TestMethod()]
+        public void ConstantGateTest()
+        {
+            var algo = new DeutschAlgorithm();
+
+            Assert.IsFalse(algo.IsBalanced(new ConstantGate()));
+        }
+
+        public class BalancedGate : BinaryOperation
+        {
+            protected override Complex[,] GetMatrix()
+            {
+                return new Complex[,]
+                {
+                    { 0, 0, 1, 0 },
+                    { 0, 1, 0, 0 },
+                    { 1, 0, 0, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+        }
+
+        public class ConstantGate : BinaryOperation
+        {
+            protected override Complex[,] GetMatrix()
+            {
+                return new Complex[,]
+                {
+                    { 1, 0, 0, 0 },
+                    { 0, 1, 0, 0 },
+                    { 0, 0, 1, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+        }
+    }
+}
---
+++ Tcgv.QuantumSim/Algorithms/DeutschAlgorithms.cs
@@ -0,0 +1,23 @@
+﻿using Tcgv.QuantumSim.Data;
+using Tcgv.QuantumSim.Operations;
+
+namespace Tcgv.QuantumSim.Algorithms
+{
+    public class DeutschAlgorithm
+    {
+        public bool IsBalanced(BinaryOperation gate)
+        {
+            var q1 = new Qubit(false);
+            var q2 = new Qubit(true);
+
+            new HGate().Apply(q1);
+            new HGate().Apply(q2);
+
+            gate.Apply(q2, q1);
+
+            new HGate().Apply(q1);
+
+            return q1.Measure();
+        }
+    }
+}
